ec05a2baf3266f50ea4efbeda3368e2e16d601a2
Merit Student <student@meac.local>
2018-09-12 13:31:03 -0600

    take this home plz

--- textadventure.py
+++ textadventure.py
@@ -1,1 +1,11 @@
-print("working")
+print("To start type \'I'M READY\' in all caps or \'QUIT\' to exit!!")
+fromUser = input()
+while True:
+    if fromUser == "I'M READY":
+        print("Let the games begin!")
+    elif fromUser == "QUIT":
+        print("Game Over!!")
+        break
+    else:
+        print("TYPE I'M READY OR YOU CAN'T PLAY!!")
+    fromUser = input()
